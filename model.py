77852b6cd1150dfdefd6fdb4c9fa82a11e67dad4
Joshua Everett <jeverer@gmail.com>
2026-09-22 15:08:15 +0100

    Step 2: split_prng_key

--- model.py
+++ model.py
@@ -16,8 +16,13 @@ def make_prng_key(seed):
     key = jax.random.PRNGKey(seed)
     return key
 
-# Step 2 - split_prng_key (not yet solved)
-# TODO: implement
+# Step 2 - split_prng_key
+import jax
+
+def split_prng_key(key, num):
+    # TODO: split `key` into `num` independent subkeys and return them as a (num, 2) array.
+    arr = jax.random.split(key, num)
+    return arr
 
 # Step 3 - sample_normal_matrix (not yet solved)
 # TODO: implement
