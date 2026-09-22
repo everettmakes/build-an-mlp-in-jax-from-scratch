e3efe4fbb770671eefdd4a9b557b91a9aacd1cf5
Joshua Everett <jeverer@gmail.com>
2026-09-22 15:48:17 +0100

    Step 8: init_mlp_params

--- model.py
+++ model.py
@@ -64,8 +64,6 @@ def init_mlp_params(key, layer_sizes, scale=0.1):
 
 # Step 9 - linear_forward
 def linear_forward(x, layer_params):
-    # TODO: compute x @ W + b using layer_params['W'] and layer_params['b'].
-
     output = x @ layer_params['W'] + layer_params['b']
     return output
 
